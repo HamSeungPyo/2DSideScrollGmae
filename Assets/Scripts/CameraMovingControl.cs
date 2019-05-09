416f5a43b7962f468900f9e953bf6a4212078f00
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-05-09 19:03:10 +0900

    기본

---
+++ Assets/Scripts/CameraMovingControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovingControl : MonoBehaviour
+{
+    float speed = 5;
+    Vector2 move=new Vector2();
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        
+        //move.y = Input.GetAxisRaw("");
+
+        transform.Translate(move * Time.deltaTime * speed);
+    }
+    public float GetMoveX()
+    {
+        return move.x;
+    }
+}
---
+++ Assets/Scripts/MapMovingControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMovingControl : MonoBehaviour
+{
+    public GameObject[] mountains = new GameObject[4];
+    public GameObject graveyard;
+    public GameObject cameraObj;
+    CameraMovingControl script_cameraObj;
+    float movingSpeed = 1;
+    public bool bMovingStop = false;
+
+    float scrollSpeed = 0.1f;
+
+    public MeshRenderer rendererMat;
+    float Offset;
+
+    private void Start()
+    {
+        script_cameraObj = cameraObj.GetComponent<CameraMovingControl>();
+    }
+    void Update()
+    {
+        if (!bMovingStop)
+        {
+            movingSpeed = script_cameraObj.GetMoveX();
+            MountainIterativeDeployment();
+            Offset -= Time.deltaTime * (movingSpeed * scrollSpeed);
+            rendererMat.material.mainTextureOffset = new Vector2(Offset, 0);
+        }
+    }
+
+    void MountainIterativeDeployment()
+    {
+        transform.localPosition = new Vector2(cameraObj.transform.localPosition.x, 0);
+        foreach (GameObject obj in mountains)
+        {
+            if (obj.transform.localPosition.x<=-24)
+            {
+                obj.transform.localPosition = new Vector2(14.4f, -1.96f);
+            }
+            else if (obj.transform.localPosition.x >= 24)
+            {
+                obj.transform.localPosition = new Vector2(-14.4f, -1.96f);
+            }
+            obj.transform.Translate(Vector2.left * Time.deltaTime * movingSpeed);
+        }
+    }
+}
